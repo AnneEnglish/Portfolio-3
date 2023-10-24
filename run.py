7a0eb52db936f6a1ffa3f4cdad86982cfc0a94cc
AnneEnglish <>
2023-10-24 15:59:59 +0000

    imported my survey to use for run.py

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pandas as pd
+
+dataframe = pd.read_csv('survey_results.csv')
+
+print(dataframe)
