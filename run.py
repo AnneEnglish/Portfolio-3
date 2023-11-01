2139a1775597e0ff5cdf5f4715e1341618a323d1
AnneEnglish <>
2023-11-01 22:35:59 +0000

    Changed most code in run.py as I couldn't get it to work

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 # Import the necessary libraries
 import pandas as pd
-
 import click
-
 import gspread
 
-# Google Sheets API scope and credentials
+# Google Sheets API Credentias and Scope
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -13,42 +11,40 @@ SCOPE = [
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/drive.file'
 ]
-
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-# @click.group decorator from click library
-# This decorator is used to define a group of commands in a CLI application
-@click.group()
-def cli():
-    # Placeholder that does nothing
-    pass
 
-@cli.command()
-@click.argument('survey_results', type=str, help='Survey Results')
-@click.argument('survey_results_output', type=str, help='CSV file for analyzing results')
-def import_data(survey_results, survey_results_output):
+def import_data(survey_results):
+    """
+    This function gets the survey data by sheet name.
+    It then returns that data as a Pandas DataFrame
+    """
     try:
-        SHEET = GSPREAD_CLIENT.open('survey_results')
-        results = SHEET.worksheet('results')
-        data = results.get_all_values()
-        # Getting the data for DataFrame without the headers
-        dataframe = pd.DataFrame(data[1:], columns=data[0])
-        # Save DataFrame to CSV file, excluding row index
-        dataframe.to_csv(survey_results_output, index=False)
-        print(f"Data imported from {survey_results} and exported to {survey_results_output}.")
-    except Error as e:
-        print(f"Error importing data from {str(e)}!")
+        sheet = GSPREAD_CLIENT.open('survey_results')
+        worksheet = sheet.get_worksheet(0)
+        survey_data = worksheet.get_all_records()
+        return pd.DataFrame(survey_data)
+    except Exception as e:
+        raise click.ClickException(f"Error importing data: {str(e)}")
 
 
-# Converting the data to a Pandas Dataframe
-dataframe = pd.DataFrame(data, columns=data[0])
+def analyze_data(df):
+    """
+    This function analyzes the survey data.
+    """
+    print(df.describe())
+    print(df['Age'].value_counts())
 
-# Calculate statistics
-summary_statistics = dataframe.describe()
 
-print(summary_statistics)
+# Click command to run the analysis
+@click.command()
+@click.option('--survey_results', prompt='Enter Google Sheet name', help='Name of sheet containing survey data')
+def analyze(survey_results):
+    df = import_data(survey_results)
 
-# Analyzing data
-def analyze_data(data)
+
+if __name__ == '__main__':
+    analyze()
+    
